d35de2774cd3e2521d7a44ebc2b7c2929164efc9
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-16 23:58:24 +0500

    Created about view, template and url pattern

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "restaurant/index.html")
+
+
+def about(request):
+    return render(request, "restaurant/about.html")
