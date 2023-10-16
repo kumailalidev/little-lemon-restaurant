1b05ab829a026663d127f025f93cd96c4db8a8b6
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-17 00:53:40 +0500

    Created database models Booking and Menu for stroing booking information and menu items respectively.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Booking(models.Model):
+    """
+    Model for booking information.
+    """
+
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Menu(models.Model):
+    """
+    Model for menu items.
+    """
+
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    menu_item_description = models.TextField(max_length=1000, default="")
+
+    def __str__(self):
+        return self.name
