1f5304cb63a68f1206db9435324c94e7b3763b3e
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-17 19:29:09 +0500

    Created bookings view to create bookings and return json response

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Menu
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.core import serializers
+
+from .models import Menu, Booking
 from .forms import BookingForm
+
+from datetime import datetime
+import json
 
 
 def home(request):
@@ -38,3 +45,46 @@ def book(request):
     }
 
     return render(request, "restaurant/book.html", context)
+
+
+@csrf_exempt
+def bookings(request):
+    """
+    Bookings view for adding bookings to database and returning the JSON
+    object containing current bookings present in database based on provided
+    date.
+    """
+
+    if request.method == "POST":
+        # get the JSON format data
+        data = json.load(request)
+
+        # returns True if QuerySet exists else returns False
+        exist = (
+            Booking.objects.filter(reservation_date=data["reservation_date"])
+            .filter(reservation_slot=["reservation_slot"])
+            .exists()
+        )
+        # create a booking if there is empty slot else return error.
+        if exist == False:
+            # create an booking object in database
+            booking = Booking(
+                first_name=data["first_name"],
+                reservation_date=data["reservation_date"],
+                reservation_slot=data["reservation_slot"],
+            )
+            booking.save()
+        else:
+            return HttpResponse("{'error': 1}", content_type="application/json")
+
+    # get date request parameter, if it doesn't exists assign python datetime value
+    date = request.GET.get("date", datetime.today().date())
+
+    # get booking QuerySet, filtered based on current date
+    bookings = Booking.objects.all().filter(reservation_date=date)
+
+    # convert bookings QuerySet object into JSON format
+    booking_json = serializers.serialize("json", bookings)
+
+    # return HTTP response contains JSON object.
+    return HttpResponse(booking_json, content_type="application/json")
